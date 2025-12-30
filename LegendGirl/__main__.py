9de6542406f54bfac02f2cb062ef1d64d8e91611
Kannadiga XD <128951142+kannadigaXD@users.noreply.github.com>
2025-12-30 23:16:28 +0530

    Update __main__.py

--- LegendGirl/__main__.py
+++ LegendGirl/__main__.py
@@ -1,25 +1,11 @@
 import asyncio
-import platform
+from legendbot.start_bot import start_bot
 
-from LegendBS.start_bot import start_bot
-from pyrogram import __version__ as py_version
-from pyrogram import idle
+if __name__ == "__main__":
+    try:
+        loop = asyncio.get_event_loop()
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
 
-from . import *
-
-
-def Start_BotSpam():
-    for i in range(1, 26):
-        var = globals()[f"Client{i}"]
-        if var is not None:
-            start_bot(var)
-    print("➖➖➖➖➖➖➖➖➖➖➖➖")
-    print(f"🔥 Bot Spam 🔥[INFO] : Group Username {group_username}")
-    print(f"🔥 Bot Spam 🔥[INFO] : Version - {platform.python_version()}")
-    print(f"🔥 Bot Spam 🔥[INFO]: SpamBot Version - {version}")
-    print(f"🔥 Bot Spam 🔥[INFO]: Pyrogram Version - {py_version}")
-    print("➖➖➖➖➖➖➖➖➖➖➖➖")
-    idle()
-
-
-asyncio.run(Start_BotSpam())
+    loop.run_until_complete(start_bot())
